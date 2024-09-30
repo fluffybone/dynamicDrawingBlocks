0649769a0f49a9c38040248ebaed148380575398
Gaykova Polina <p.gaykova@rambler-co.ru>
2024-09-30 19:06:08 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import classes from "./app.module.css";
+
+export const App = () => {
+  return (
+    <div className={classes.wrapper}>
+      <h1>Dunamick Drawing Blocks</h1>
+    </div>
+  );
+};
+
+export default App;
